679f85e38b339368999b123af958cbbc8bd3e064
AdrianChris <chrismadegwa4@gmail.com>
2023-05-15 07:13:48 -0700

    main header file

--- main.h
+++ main.h
@@ -8,9 +8,19 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#define MAX_COMMAND
 
+char** process_input(const char* shellptr, const char* delim, int* argc);
+
+void prompt_input(char *shellptr, ssize_t *shelly_input);
+/** Execute commands  **/
+void child_process(char **argv);
+/** Free mememory **/
+
+void free_memory(char **argv, char *shellptr, char *shellptr_new, int count);
 /** Prototypes to be used. getline function **/
-void shelly_prompt(char **av, char **env);
+void shelly_prompt();
+/*void shelly_prompt(char **av, char **env);*/
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 char *strtok(char *str, const char *delim);
 char *strcpy(char *dest, const char *src);
